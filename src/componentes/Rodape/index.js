b34e372698e760279f3df75b7ccc27d84ca84a31
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2023-07-20 09:58:39 -0300

    Alteração no componente 'Rodape' - Melhoria na responsividade

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,18 +1,18 @@
 const Rodape = () => {
   return (
-    <section className="relative flex w-full h-64 overflow-hidden bg-primary items-center">
+    <section className="relative flex w-full h-64 overflow-hidden bg-primary items-center justify-center items-center">
         <img src="/imagens/fundo.png" className="h-full w-full object-cover" alt="bg footer"/>
-      <div className="absolute flex justify-between w-full items-center">
-        <div className="flex h-8 ml-36">
-          <img src="/imagens/fb.png" alt="Facebook logo" className="mr-16" />
-          <img src="/imagens/tw.png" alt="Twitter logo" className="mr-16" />
+      <div className="container absolute flex flex-wrap justify-evenly items-center place-content-center">
+        <div className="flex h-6 justify-center">
+          <img src="/imagens/fb.png" alt="Facebook logo" className="mr-6" />
+          <img src="/imagens/tw.png" alt="Twitter logo" className="mr-6" />
           <img src="/imagens/ig.png" alt="Instagram logo" />
         </div>
-        <div>
+        <div className="justify-center flex h-12">
           <img src="/imagens/logo.png" alt="Logo Alura"/>
         </div>
-        <div>
-          <h5 className="mr-40 text-white font-medium text-xl">Desenvolvido por Alura.</h5>
+        <div className="justify-center flex">
+          <h5 className="text-white font-medium text-xl">Desenvolvido por Alura.</h5>
         </div>
       </div>
     </section>
